5d669e01d91af053bd03c809f1a266e6ec2554c9
Joel <joelaranha17@gmail.com>
2017-10-25 14:38:16 -0500

    Initial commit learning github part2

---
+++ app/src/main/java/com/firstapp/joel/mygitapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.firstapp.joel.mygitapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void pushFxn()
+    {
+
+    }
+        //Intent intent =
+             //   new Intent(
+                 //       this,
+                   //     MySubActivity.class );
+
+        //startActivityForResult( intent, MY_REQUEST_CODE );
+    }
